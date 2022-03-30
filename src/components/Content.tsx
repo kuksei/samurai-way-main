a331ceda900f6e37e470c28e0fd1b1fed74198b6
kuksei <artemkuksak12@gmail.com>
2022-03-30 22:32:13 +0300

    added content,header,navbar components

---
+++ src/components/Content.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export const Content = () => {
+    return (
+        <div className="content">
+            <div className="background-img">
+                <img
+                    src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+                    alt="background"/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+};
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://cdn.logo.com/hotlink-ok/logo-social.png" alt="logo"/>
+        </header>
+    )
+};
